2a88e5f0a053f2dc1525894e3f7f77aae84ad095
David Aronchick <aronchick@gmail.com>
2020-03-31 12:06:16 -0700

    Fixed pylint errors for mlobjects and metadatavalidators.

--- mlspeclib/metadatavalidator.py
+++ mlspeclib/metadatavalidator.py
@@ -1,49 +1,63 @@
-import sys
-import re
-import uuid
-import semver as sv
+""" Validates metadata submissions by extending cerberus Validator
+    with field specific validators. """
 
+import semver as sv
 import validators
 import uritools
+import strictyaml
 
-import cerberus
 from cerberus import Validator
+from ruamel.yaml import YAML
 
-from dateutil.parser import parse as dtparse
+from dateutil.parser import ParserError, parse as dtparse
 
 from mlspeclib.helpers import check_and_return_schema_type_by_string
 
-import strictyaml
-from ruamel.yaml import YAML
-
 class MetadataValidator(Validator):
+    """ Class of all the metadata validators """
+
     def _validate_type_semver(self, value):
+        """ Uses the semver library to validate Semantic Version. Returns True/False """
         return sv.VersionInfo.isvalid(value)
-    
+
     def _validate_type_uuid(self, value):
+        """ Uses the validators library to validate UUID. Returns True/False """
         return validators.uuid(value)
 
     def _validate_type_datetime(self, value):
+        """ Uses the dateutils library to validate date time. Returns True/False """
         try:
             dtparse(value)
             return True
-        except:
+        except ParserError:
             # error(field, "Not a datetime")
             return False
 
+    #pylint: disable=invalid-name
     def _validate_type_URI(self, value):
+        """ Uses the dateutils library to validate date time. Returns True/False """
         return uritools.isuri(value)
 
-    def _validate_type_metadata(self, value):
-        return True
-
     def _validate_type_allowed_schema_types(self, value):
+        """ Validates that the schema provided in the metadata file matches a
+            schema provided. Returns True/False """
         return check_and_return_schema_type_by_string(value)
 
     def convert_and_validate(self, schema_to_check_in_text, schema_from_catalog_in_yaml):
+        """ Converts text into yaml, and verifies against schema.
+
+        schema_to_check_in_text = Text string from disk. Does validation with
+                                  strictyaml and ruamel libraries, but no checking
+                                  for safe strings.
+        schema_from_catalog_in_yaml = Yaml already added to a SchemaCatalog from mlspeclib/data.
+
+        Returns True/False.
+        """
+
         strictyaml.load(schema_to_check_in_text)
 
         ruamel_yaml = YAML()
         parsed_proposed_yaml = ruamel_yaml.load(schema_to_check_in_text)
 
         self.validate(parsed_proposed_yaml, schema_from_catalog_in_yaml)
+        
--- mlspeclib/mlobjects.py
+++ mlspeclib/mlobjects.py
@@ -1,22 +1,19 @@
-import sys
-import collections
-
+""" ML Objects created by metadata passed in based on schema in mlspeclib/data """
 from abc import ABC, abstractmethod
 
-from mlspeclib.schemaenums import SchemaTypes
-
-#pylint: disable=abstract-class-instantiated
 class GenericMLObject(ABC):
+    """ Abstract class of an MLObject. Needs schema_enum populated to be inherited. """
     @property
     @abstractmethod
-    def schema_enum(self): 
-        pass 
+    def schema_enum(self):
+        """ Abstract property to override when the specific object is instanted
+        so it can return the SchemaType from schemaenums."""
 
 class MLObjects(dict):
-# Trying to figure out a better way to do this rather than reading in everything from disk every time
-# this is instantiated. Oh well.
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
+    """ Portfolio of all objects instantiated as a dict. """
+    # Trying to figure out a better way to do this rather
+    # than reading in everything from disk every time
+    # this is instantiated. Oh well.
     def load_object(self, verified_object_yaml):
-        pass
+        """Converts verified yaml to an object. Does NOT add it to the dict.
+        This does not verification of the yaml against schema."""
