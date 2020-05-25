14db678e66aefec59b5455d33e41829d3001cccf
aronchick <aronchick@gmail.com>
2020-05-25 14:01:45 -0700

    adding build submit package

--- build_submit_package.sh
+++ build_submit_package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pipenv shell
-python3 -m pip install --user --upgrade setuptools wheel
+python3 -m pip install --upgrade setuptools wheel
 rm -rf dist/
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --upgrade twine
